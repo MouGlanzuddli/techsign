fbcee131852007ff3d124dc4d391e722347ae94d
hhqan <hhqan61@gmail.com>
2025-07-07 13:18:50 +0700

    hhqan

--- src/java/config/Constants.java
+++ src/java/config/Constants.java
@@ -1,7 +1,7 @@
 package config;
 
 public class Constants {
- 
+
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/JobSearchManagement/LoginGoogleHandler";
     public static final String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
     public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v3/userinfo";
